93402ef1fc90fa997ff88c9b0ea6456a61d0bfd6
Ujwal1830 <yangalwarujwal1830@gmail.com>
2025-09-29 00:48:52 +0530

    wipe functionality

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <nav className={"navbar"}>
             <Link to={"/"}>
-                <p className={"text-2xl font-bold text-gradient"}>Resume Analyzer</p>
+                <p className={"text-2xl font-bold text-gradient"}>Applylytics</p>
             </Link>
             <Link to={"/upload"} className={"primary-button w-fit"}>
                 Upload Resume
