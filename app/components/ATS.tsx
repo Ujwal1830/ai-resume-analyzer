3719c2eda437aa54444b171640113f639fabc1b1
Ujwal1830 <yangalwarujwal1830@gmail.com>
2025-09-28 23:56:14 +0530

    Resume Feedback page

---
+++ app/components/ATS.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface Suggestion {
+  type: "good" | "improve";
+  tip: string;
+}
+
+interface ATSProps {
+  score: number;
+  suggestions: Suggestion[];
+}
+
+const ATS: React.FC<ATSProps> = ({ score, suggestions }) => {
+  return (
+    <div>ATS</div>
+  )
+}
+
+export default ATS
